4424aa5d7433b873badfdc04ff8fd5790ef70cff
Kashan Hussain <kashanhussain857@gmail.com>
2025-07-16 13:47:33 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//========================================= Typing Animation =======================================//
+var typed = new Typed(".typing", {
+    strings: ["Undergraduate Software Engineer", "Full Stack Web Developer", "MS Office Expert", "SEO Expert", "To be Continue......."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
